9951b3416f4ab81b00c6bcda11e80f10cd966804
VeronicaLopez <lopez.vero.09@gmail.com>
2018-01-10 15:52:32 -0800

    Updated header file

--- VeronicaLopezNanogram.h
+++ VeronicaLopezNanogram.h
@@ -9,6 +9,12 @@ using namespace std;
 
 int rCord, cCord;
 char currentBoard[5][5];
+int winRow[13] = { 0,1,2,3,4,1,2,1,0,1,2,3,4 };
+int winCol[13] = { 0,0,0,0,0,1,2,3,4,4,4,4,4 };
+
+int emptyRow[12] = { 0,2,3,4,0,1,3,4,0,2,3,4 };
+int emptyCol[12] = { 1,1,1,1,2,2,2,2,3,3,3,3 };
+
 
 /*int validCheck()
 {
@@ -23,11 +29,24 @@ char currentBoard[5][5];
 */
 void printBoard()
 {
-	cout << " ___ ___ ___ ___ ___" << endl;
+	cout << "      _5_ _1_ _1_ _1_ _5_" << endl;
 	for (int row = 0; row < 5; row++)
 	{
+		if (row == 2)
+		{
+			cout << "1 1 1";
+		}
+		if (row == 1)
+		{
+			cout << "  2 2";
+		}
+		if (row == 0|| row == 3 || row == 4)
+		{
+			cout << "  1 1";
+		}
 		for (int column = 0; column < 5; column++)
 		{
+			
 			cout << "|_";
 			cout << currentBoard[row][column];
 			cout << "_";
@@ -78,7 +97,7 @@ void userInput()
 	displayCurrentBoard();
 }
 
-void remove()
+void remove() //takes in new coordinates for row and column and removed the X value
 {
 	cout << "Enter row coordinate (0-4): ";
 	cin >> rCord;
@@ -86,8 +105,39 @@ void remove()
 	cout << "Enter column coordinate (0-4): ";
 	cin >> cCord;
 
-	currentBoard[rCord][cCord] = '_';
-	printBoard();
+	currentBoard[rCord][cCord] = '_'; //replace any value to underscore
+	printBoard(); 
 }
 
+bool checkWin()
+{
+	int count = 0;
+	bool win;
+	for (int i = 0; i < 13; i++)
+	{
+		if (currentBoard[winRow[i]][winCol[i]] == 'X')
+		{
+			count++;
+		}
+	}
+	if (count == 13)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (currentBoard[emptyRow[j]][emptyCol[j]] == 'X')
+			{
+				win = false;
+				return win;
+			}
+		}
+		win = true;
+		return win;
+	}
+	else
+	{
+		win = false;
+		return win;
+	}
+		
+}
 #endif
