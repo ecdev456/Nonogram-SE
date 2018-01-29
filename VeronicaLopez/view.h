8a50c421edbf8f46fee9640a067035dbcba439ed
VeronicaLopez <lopez.vero.09@gmail.com>
2018-01-29 13:10:50 -0800

    Add files via upload
    Updated files to incorporate Edwin and Faraz code in model.h

--- VeronicaLopez/view.h
+++ VeronicaLopez/view.h
@@ -1,33 +1,42 @@
 #pragma once
+#ifndef view_h
+#define view_h
 #include <iostream>
 #include <Windows.h>
+#include "model.h"
 
 using namespace std;
+extern int actNum[5][5] =
+{
+	{ 1,1,1,1,1 },
+	{ 1,1,1,1,1 },
+	{ 1,1,1,1,1 },
+	{ 1,1,1,1,1 },
+	{ 1,1,1,1,1 }
+};
+extern int rulloGrid[5][5] =
+{
+	{ 3,2,6,5,2 },
+	{ 3,7,1,1,7 },
+	{ 5,1,5,2,6 },
+	{ 2,9,6,6,7 },
+	{ 2,6,5,2,4 }
+};//rullo grid
 
 class view {
-private:
+	friend class model;
 	static const int size = 5;
-	int actNum[size][size] =
-	{
-		{1,1,1,1,1},
-		{1,1,1,1,1},
-		{1,1,1,1,1},
-		{1,1,1,1,1},
-		{1,1,1,1,1}
-	};
+
+private:
+	
 	char picrossGrid[size][size];//stores information of picross grid
-	int rulloGrid[size][size] = 
-	{
-		{ 3,2,6,5,2 },
-		{ 3,7,1,1,7 },
-		{ 5,1,5,2,6 },
-		{ 2,9,6,6,7 },
-		{ 2,6,5,2,4 }
-	};//rullo grid
-	int hSum[size] = { 9,17,13,12,15 };
-	int vSum[size] = { 13,16,12,23,02 };
+
+	int vValues[size] = { 13,16,12,23,02 };
+	int hValues[size] = { 9,17,13,12,15 };
 	int rowP, columnP; //picross coordinate selection
 	int rowRullo, columnRullo; //rullo coordinate selection
+	//int hSum[size] = { 0,0,0,0,0 };
+	//int vSum[size] = { 0,0,0,0,0 };
 public:
 	view()
 	{
@@ -89,7 +98,7 @@ public:
 			cin >> columnRullo;
 		}
 		actNum[rowRullo][columnRullo] = 0;
-		printRullo();
+		//printRullo();
 	}
 
 	//prints Picross Grid
@@ -148,23 +157,41 @@ public:
 		cout << "    ";
 		for (int i = 0; i < size; i++)
 		{
-			cout << hSum[i] << "  ";
+			if (vAct[i] == 1)
+			{
+				SetConsoleTextAttribute(hConsole, 3);
+				cout << hValues[i] << "  ";
+				SetConsoleTextAttribute(hConsole, 2);
+			}
+			else
+			{
+				cout << hValues[i] << "  ";
+			}
 		}
 		cout << endl;
 
 		for (int row = 0; row < 5; row++)
 		{
-			cout << vSum[row];
+			if (hAct[row] == 1)
+			{
+				SetConsoleTextAttribute(hConsole, 3);
+				cout << vValues[row];
+				SetConsoleTextAttribute(hConsole, 2);
+			}
+			else
+			{
+				cout << vValues[row];
+			}
+			
 			if (row == 4)
 			{
 				cout << " ";
 			}
 			for (int column = 0; column < 5; column++)
 			{
-				
+
 				cout << "|_";
-				//cout << rulloGrid[row][column];
-				
+
 				if (actNum[row][column] == 0)
 				{
 					SetConsoleTextAttribute(hConsole, 1);
@@ -182,4 +209,58 @@ public:
 			cout << endl;
 		}
 	}
+	void removePicross() //takes in new coordinates for row and column and removed the X value
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		cout << "Enter row coordinate (0-4): ";
+		cin >> rowP;
+		while (rowP < 0 || rowP > 4) //checks to see if you have entered a valid input for row coordinate
+		{
+			cout << "Invalid Input" << endl;
+			cout << "Enter row coordinate (0-4): ";
+			cin >> rowP;
+		}
+
+		cout << "Enter column coordinate (0-4): ";
+		cin >> columnP;
+		while (columnP < 0 || columnP > 4) //checks to see if you have entered a valid input for column coordinate
+		{
+			cout << "Invalid Input" << endl;
+			cout << "Enter column coordinate (0-4): ";
+			cin >> columnP;
+		}
+		SetConsoleTextAttribute(hConsole, 2);
+		picrossGrid[rowP][columnP] = '_'; //replace any value to underscore
+		printPicross();
+		
+		//checkM.CheckMath();
+	}
+
+	void removeRullo() //takes in new coordinates for row and column and removed the X value
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		cout << "Enter row coordinate (0-4): ";
+		cin >> rowRullo;
+		while (rowRullo < 0 || rowRullo > 4) //checks to see if you have entered a valid input for row coordinate
+		{
+			cout << "Invalid Input" << endl;
+			cout << "Enter row coordinate (0-4): ";
+			cin >> rowRullo;
+		}
+
+		cout << "Enter column coordinate (0-4): ";
+		cin >> columnRullo;
+		while (columnRullo < 0 || columnRullo > 4) //checks to see if you have entered a valid input for column coordinate
+		{
+			cout << "Invalid Input" << endl;
+			cout << "Enter column coordinate (0-4): ";
+			cin >> columnRullo;
+		}
+		SetConsoleTextAttribute(hConsole, 2);
+		actNum[rowRullo][columnRullo] = 1;
+		rulloGrid[rowRullo][columnRullo];// = '_'; //replace any value to underscore
+		//printRullo();
+	}
+
 };
+#endif
