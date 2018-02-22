639c4c5c9208ff3ddaf5b0a4bd80b50d12003693
tandrews4 <35308066+tandrews4@users.noreply.github.com>
2018-02-21 20:43:44 -0800

    Add files via upload

---
+++ Project 4/checkout.cpp
@@ -0,0 +1,19 @@
+#include "checkout.h"
+#include "ui_checkout.h"
+
+Checkout::Checkout(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Checkout)
+{
+    ui->setupUi(this);
+}
+
+Checkout::~Checkout()
+{
+    delete ui;
+}
+
+void Checkout::on_buttonBox_accepted()
+{
+    close();
+}
